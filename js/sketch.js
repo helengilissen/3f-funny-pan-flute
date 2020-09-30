306f6a42d964fd579702539f0cf2cd18cd793bfc
Helen Gilissen <55249087+helengilissen@users.noreply.github.com>
2020-09-30 10:26:14 +0200

    added 3f funny pan flute

---
+++ js/sketch.js
@@ -0,0 +1,24 @@
+// 3B: Funny pan flute
+
+function setup () {
+  createCanvas (300,300)
+  background (240)
+}
+
+function draw () {
+    strokeWeight (1)
+    stroke (255,150,0)
+    line (50,50, 50, 250)
+
+    strokeWeight (5)
+    stroke (255,150,0)
+    line (100 ,50, 100, 200, 100)
+
+    strokeWeight (1)
+    stroke (255,150,0)
+    line (150,50, 150, 150)
+
+    strokeWeight (5)
+    stroke (255,150,0)
+    line (200,50, 200, 100, 100)
+}
